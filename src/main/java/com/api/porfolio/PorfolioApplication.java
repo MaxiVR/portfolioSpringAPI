9546a5e55a36d5796322105ddb5139a97d457637
Maximiliano Vazquez <maximilianovazquez88@gmail>
2022-05-20 17:09:23 -0300

    api

--- src/main/java/com/api/porfolio/PorfolioApplication.java
+++ src/main/java/com/api/porfolio/PorfolioApplication.java
@@ -2,9 +2,11 @@ package com.api.porfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
-
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST, RequestMethod.PATCH, RequestMethod.PUT, RequestMethod.DELETE})
 public class PorfolioApplication {
 
 	public static void main(String[] args) {
