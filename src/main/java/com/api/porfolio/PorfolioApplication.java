d949bdfcdf54df08635f29dc00a758898edb4756
Maximiliano Vazquez <maximilianovazquez88@gmail>
2022-05-25 20:36:30 -0300

    as

--- src/main/java/com/api/porfolio/PorfolioApplication.java
+++ src/main/java/com/api/porfolio/PorfolioApplication.java
@@ -3,7 +3,6 @@ package com.api.porfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
