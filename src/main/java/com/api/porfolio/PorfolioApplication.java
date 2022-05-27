2b5d4a36608ac0204d3c4d3fb96cc5496f886542
Maximiliano Vazquez <maximilianovazquez88@gmail>
2022-05-27 20:53:32 -0300

    asdf2

--- src/main/java/com/api/porfolio/PorfolioApplication.java
+++ src/main/java/com/api/porfolio/PorfolioApplication.java
@@ -16,7 +16,7 @@ public class PorfolioApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PorfolioApplication.class, args);
 	}
-        
+        /*
         @Bean
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
@@ -31,6 +31,6 @@ public class PorfolioApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
 		return new CorsFilter(urlBasedCorsConfigurationSource);
-	}
+	}*/
 
 }
